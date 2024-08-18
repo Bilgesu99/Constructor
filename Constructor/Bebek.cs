c07a59c4e0c436dd4a17de0ece055596e6075ede
asus <asus@DESKTOP-OTTG4TB>
2024-08-18 18:08:59 +0300

    Add project files.

---
+++ Constructor/Bebek.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Bebek
+    {
+        public DateTime DoğumTarihi { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+
+         // ilk olarak default Constructor yazıyoruz
+         public Bebek()
+        {
+            DoğumTarihi = DateTime.Now;
+            Console.WriteLine("Ingaa");
+        }
+
+        //Diğer Constructor
+        public Bebek (string ad, string soyad)
+        {
+            // Konsola "Ingaaaa" yazdır
+            Console.WriteLine("Ingaaaa");
+            // Parametreleri property'lere ata
+            Ad = ad;
+            Soyad = soyad;
+            // Doğum Tarihini o anki tarih olarak ata
+            DoğumTarihi = DateTime.Now;
+        }
+
+        public void Yazdırma()
+        {
+            Console.WriteLine("Ad: " + Ad);
+            Console.WriteLine("Soyad: " + Soyad);
+            Console.WriteLine("Doğum Tarihi: " + DoğumTarihi.ToShortDateString());
+        }
+
+
+
+
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+namespace Constructor
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Bebek bebek1 = new Bebek();
+            Console.WriteLine("Bebek 1 Bilgileri:");
+            bebek1.Yazdırma();
+            Console.WriteLine();
+
+            // Parametreli constructor ile bebek nesnesi oluşturma
+            Bebek bebek2 = new Bebek("Ali", "Veli");
+            Console.WriteLine("Bebek 2 Bilgileri:");
+            bebek2.Yazdırma();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
